8aaf09e06736726b0e87d7fec4076d3788fc6306
achernecova <achernecova1904@gmail.com>
2025-12-17 23:01:13 +0300

    HW-15: созданы фикстуры с параметрами. Созданы тесты в которых есть переопределение через indirect, скип теста в зависимости от типа (мобильное или десктоп)

--- qa_guru_homework_python_lesson15/tests/conftest.py
+++ qa_guru_homework_python_lesson15/tests/conftest.py
@@ -11,9 +11,6 @@ from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from softmg_site.utils import attach
-
-
 # DEFAULT_BROWSER_VERSION = "127.0"
 #
 #
@@ -60,26 +57,118 @@ from softmg_site.utils import attach
 #     attach.add_video(browser)
 #
 #     browser.quit()
-@pytest.fixture(scope="function", autouse=True)
-def driver_setup():
-    '''Фикстура для запуска локально'''
-    chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.page_load_strategy = "none"
-    chrome_options.add_argument("--start-maximized")
+# @pytest.fixture(scope="function", autouse=True)
+# def driver_setup():
+#     '''Фикстура для запуска локально'''
+#     chrome_options = Options()
+#     # chrome_options.add_argument("--headless")
+#     # chrome_options.page_load_strategy = "none"
+#     chrome_options.add_argument("--start-maximized")
+#
+#     # Todo - для firefox другие переменные передаются в расширение:
+#     # chrome_options.add_argument("--width=1920")
+#     # chrome_options.add_argument("--height=1080")
+#     driver = webdriver.Chrome(options=chrome_options)
+#
+#     # Передаем драйвер в Selene
+#     browser.config.driver = driver
+#
+#     yield driver
+#     browser.driver.maximize_window()
+#     # attach.add_screenshot(browser)
+#     # attach.add_logs(browser)
+#     # attach.add_html(browser)
+#     # attach.add_video(browser)
+#     browser.quit()
 
-    # Todo - для firefox другие переменные передаются в расширение:
-    # chrome_options.add_argument("--width=1920")
-    # chrome_options.add_argument("--height=1080")
-    driver = webdriver.Chrome(options=chrome_options)
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.safari.options import Options as SafariOptions
 
-    # Передаем драйвер в Selene
-    browser.config.driver = driver
+
+@pytest.fixture(params=["Chrome", "Firefox", "Safari"])
+def browser_setup(request):
+    if request.param == "Chrome":
+        '''Фикстура для запуска локально'''
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        # chrome_options.page_load_strategy = "none"
+        # chrome_options.add_argument("--start-maximized")
+        driver = webdriver.Chrome(options=chrome_options)
+        browser.config.driver = driver
+
+    elif request.param == "Firefox":
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        # В Firefox --start-maximized не работает. Нашла на каком-то форуме
+        # options.add_argument("--kiosk")
+        driver = webdriver.Firefox(options=options)
+        browser.config.driver = driver
+
+    elif request.param == "Safari":
+        options = SafariOptions()
+        driver = webdriver.Safari(options=options)
+        # Safari не поддерживает аргументы запуска, максимизируем через драйвер
+        # driver.maximize_window()
+        browser.config.driver = driver
 
     yield driver
-    browser.driver.maximize_window()
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    # attach.add_video(browser)
+    browser.quit()
+
+
+@pytest.fixture(scope="function", params=[(390, 844), (430, 932), (360, 740)], ids=["390*844", "430*932", "360*740"])
+def browser_mobile(request):
+    width, height = request.param
+    chrome_options = Options()
+    chrome_options.add_argument(f"--window-size={width},{height}")
+    driver = webdriver.Chrome(options=chrome_options)
+    browser.config.driver = driver
+    yield driver
+    browser.quit()
+
+
+@pytest.fixture(scope="function", params=[(1920, 1080), (1270, 500), (1366, 768)],
+                ids=["1920*1080", "1270*500", "1366*768"])
+def browser_desktop(request):
+    width, height = request.param
+    chrome_options = Options()
+    chrome_options.add_argument(f"--window-size={width},{height}")
+    driver = webdriver.Chrome(options=chrome_options)
+    browser.config.driver = driver
+    yield driver
+    browser.quit()
+
+
+@pytest.fixture(scope="function", params=[(1920, 1080), (1270, 500), (390, 844), (430, 932)],
+                ids=["1920*1080", "1270*500", "390*844", "430*932"])
+def browser_desktop_and_mobile(request):
+    width, height = request.param
+    chrome_options = Options()
+    chrome_options.add_argument(f"--window-size={width},{height}")
+    driver = webdriver.Chrome(options=chrome_options)
+    browser.config.driver = driver
+    yield driver
+    browser.quit()
+
+
+@pytest.fixture(scope="function", params=[(1920, 1080), (1270, 500), (390, 844), (430, 932)],
+                ids=["1920*1080", "1270*500", "390*844", "430*932"])
+def browser_desktop_and_mobile_skip(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+    if width > 900:
+        chrome_options = Options()
+        chrome_options.add_argument(f"--window-size={width},{height}")
+        driver = webdriver.Chrome(options=chrome_options)
+        browser.config.driver = driver
+        yield 'desktop'
+    else:
+        chrome_options = Options()
+        chrome_options.add_argument(f"--window-size={width},{height}")
+        driver = webdriver.Chrome(options=chrome_options)
+        browser.config.driver = driver
+        yield 'mobile'
     browser.quit()
---
+++ qa_guru_homework_python_lesson15/tests/test_github_parametrize_two_fixtures.py
@@ -0,0 +1,47 @@
+import pytest
+from selene import browser, be
+from selenium.webdriver.common.by import By
+
+base_url = "https://github.com/"
+mobile_locator = (By.XPATH, "//*[contains(@class, 'HeaderMenu-link HeaderMenu-button ')]")
+desktop_locator = (By.XPATH, "//*[contains(@class, 'HeaderMenu-link--sign-in')]")
+
+
+def test_parametrize_window_mobile(browser_mobile):
+    browser.open(base_url)
+    browser.element(mobile_locator).should(be.visible).click()
+
+
+def test_parametrize_window_desktop(browser_desktop):
+    browser.open(base_url)
+    browser.element(desktop_locator).should(be.visible).click()
+
+
+@pytest.mark.parametrize("browser_desktop_and_mobile", [(1920, 1080), (1270, 500)], indirect=True,
+                         ids=["1920*1080", "1270*500"])
+def test_parametrize_window_general_fixture_desktop(browser_desktop_and_mobile):
+    browser.open(base_url)
+    browser.element(desktop_locator).should(be.visible).click()
+
+
+@pytest.mark.parametrize("browser_desktop_and_mobile", [(390, 844), (430, 932)], indirect=True,
+                         ids=["390*844", "430*932"])
+def test_parametrize_window_general_fixture_mobile(browser_desktop_and_mobile):
+    browser.open(base_url)
+    browser.element(mobile_locator).should(be.visible).click()
+
+
+def test_parametrize_desktop_skip(browser_desktop_and_mobile_skip):
+    if browser_desktop_and_mobile_skip == 'desktop':
+        pytest.skip(reason="Это десктопное расширение, скипаем тест")
+
+    browser.open(base_url)
+    browser.element(mobile_locator).should(be.visible).click()
+
+
+def test_parametrize_mobile_skip(browser_desktop_and_mobile_skip):
+    if browser_desktop_and_mobile_skip == 'mobile':
+        pytest.skip(reason="Это мобильное расширение, скипаем тест")
+    else:
+        browser.open(base_url)
+        browser.element(desktop_locator).should(be.visible).click()
